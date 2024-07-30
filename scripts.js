ca4d81244dd6f1b21ed216b32812379fb373a713
falconmaverick <39731035+falconmaverick@users.noreply.github.com>
2024-07-30 10:49:54 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
-// This file can be used for future enhancements
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Website Loaded');
+// Custom Cursor
+const cursor = document.getElementById('custom-cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+});
+
+document.addEventListener('mouseenter', e => {
+    cursor.style.display = 'block';
+});
+
+document.addEventListener('mouseleave', e => {
+    cursor.style.display = 'none';
 });
